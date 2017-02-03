f94d5ef73ed96ac7a859a26f267d99bdde356598
JoTerrance <gorka.zgz@gmail.com>
2017-02-03 08:38:46 +0100

    commit primera mitad de video

---
+++ src/main/java/es/salesianos/edu/model/Author.java
@@ -0,0 +1,25 @@
+package es.salesianos.edu.model;
+
+import java.util.Date;
+
+public class Author {
+
+	private String nameAuthor;
+	private Date dateOfBirth;
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getNameAuthor() {
+		return nameAuthor;
+	}
+
+	public void setNameAuthor(String nameAuthor) {
+		this.nameAuthor = nameAuthor;
+	}
+
+}
--- src/main/java/es/salesianos/edu/webpages/HomePage.java
+++ src/main/java/es/salesianos/edu/webpages/HomePage.java
@@ -2,13 +2,31 @@ package es.salesianos.edu.webpages;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.form.datetime.DatetimePicker;
+import es.salesianos.edu.model.Author;
 
 public class HomePage extends WebPage {
 
 	public HomePage() {
-		Label label = new Label("holamundo", Model.of("buenos dias"));
-		add(label);
+
+
+		Form form = new Form("formInsertLogin", new CompoundPropertyModel(new Author()));
+		form.add(new Label("nameAuthorLabel", getString("author.name")));
+		form.add(new Label("dateOfBirthLabel", getString("date.of.birth")));
+		form.add(new RequiredTextField("nameAuthor"));
+		DatetimePicker datetimePicker = new DatetimePicker("dateOfBirth", "yyyy-MM-dd");
+		form.add(datetimePicker);
+		FeedbackPanel feedbackPanel = new FeedbackPanel("feedbackMessage");
+		feedbackPanel.setOutputMarkupId(true);
+		add(feedbackPanel);
+
+		add(form);
+
 	}
 
 }
